449a843fd6b22b554500a3d299ba79457654d08e
Katsiaryna <katsiaryna.n@gmail.com>
2023-08-03 16:54:20 +0300

    feat: add webpack configs

---
+++ webpack.config.js
@@ -0,0 +1,67 @@
+const path = require('path');
+const { merge } = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const EslintPlugin = require('eslint-webpack-plugin');
+
+const baseConfig = {
+  mode: 'development',
+  entry: path.resolve(__dirname, './src/index'),
+  module: {
+      rules: [
+          {
+              test: /\.s[ac]ss$/i,
+              use: [
+                'style-loader',
+                'css-loader',
+                'sass-loader',
+                { 
+                  loader: 'sass-resources-loader',
+                  options: {
+                    resources: [
+                      './src/style.scss'
+                    ]
+                  } }
+              ],
+            },
+          {
+              test: /\.html$/,
+              use: [
+                { loader: 'html-loader' },
+              ],
+            },
+          {
+              test: /\.ts$/i,
+              use: 'ts-loader',
+          },
+          {
+              test: /\.(?:ico|gif|png|jpg|jpeg|mp3|wav|svg)$/i,
+              type: 'asset/resource',
+          },
+          {
+              test: /\.(woff|woff2|eot|ttf|otf)$/i,
+              type: 'asset/resource',
+          },
+      ],
+  },
+  resolve: {
+      extensions: ['.ts', '.js'],
+  },
+  output: {
+      filename: 'index.js',
+      path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+      new HtmlWebpackPlugin({
+          template: path.resolve(__dirname, './src/index.html'),
+          filename: 'index.html',
+      }),
+      new EslintPlugin({ extensions: 'ts' }),
+  ],
+};
+
+module.exports = ({ mode }) => {
+  const isProductionMode = mode === 'prod';
+  const envConfig = isProductionMode ? require('./webpack.prod.config') : require('./webpack.dev.config');
+
+  return merge(baseConfig, envConfig);
+};
