2e0672f58d1611ba80c8a0702ac9436cbceeb796
Vladimir Myazin <v.myazin@gmail.com>
2023-08-11 17:40:56 +0400

    feat: move createFormBlock function to separate module, delete temporary file Fri, Aug 11, 2023  5:40:56 PM

--- src/view/pages/registration/registration.ts
+++ src/view/pages/registration/registration.ts
@@ -1,26 +1,7 @@
 import countries from '../../../model/data/countries';
 import { AddressType, FormBlock } from '../../../types/type';
+import createFormBlock from '../../../utils/view/createFormBlock';
 import resultMessage from './resultMessage';
-
-const createFormBlock = (options: FormBlock): HTMLDivElement => {
-  const block = document.createElement('div');
-  block.className = 'reg-form__block';
-  const input = document.createElement('input');
-  input.type = options.type;
-  if (options.placeholder) input.placeholder = options.placeholder;
-  input.name = options.name;
-  input.required = options.required;
-  if (options.pattern) input.pattern = options.pattern;
-  if (options.max) input.max = options.max;
-  if (options.title) input.title = options.title;
-
-  const label = document.createElement('label');
-  label.htmlFor = options.name;
-  label.textContent = options.text;
-  const errorMessage = document.createElement('p');
-  block.append(label, input, errorMessage);
-  return block;
-};
 
 const createAddressBlock = (addressType: AddressType): HTMLDivElement => {
   const block = document.createElement('div');
