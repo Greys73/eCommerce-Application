783f2e917d85e109c6bc024613ce0a071c3e6c8c
Vladimir Myazin <v.myazin@gmail.com>
2023-08-11 17:13:19 +0400

    feat: add link to main page Fri, Aug 11, 2023  5:13:18 PM

--- src/view/pages/404/404.ts
+++ src/view/pages/404/404.ts
@@ -1,7 +1,19 @@
 import img from '../../../assets/images/page_not_found404.png';
 
+const container = document.createElement('div');
+container.style.display = 'flex';
+container.style.flexDirection = 'column';
+container.style.justifyContent = 'center';
+container.style.alignItems = 'center';
+
 const image = document.createElement('img');
 image.src = img;
 image.style.height = '30rem';
 
-export default image;
+const link = document.createElement('a');
+link.textContent = 'Go back to main page!';
+link.href = '/';
+
+container.append(image, link);
+
+export default container;
