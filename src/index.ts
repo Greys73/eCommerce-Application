51226a4f2bd3e5b74d1917ef67cdacc8fca0db59
Katsiaryna <katsiaryna.n@gmail.com>
2023-08-12 11:05:58 +0300

    feat: import handlers in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
+import addErrorHandlers from './controller/regFormErrorHanlder';
 import './controller/router';
 import main from './view/view';
+import './controller/regSubmitHandler';
 
 document.body.append(main);
+addErrorHandlers();
