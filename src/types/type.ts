8b2fc4edbdf08eae6d8d2e96bae92e2e2871e29d
Katsiaryna <katsiaryna.n@gmail.com>
2023-08-25 16:13:26 +0300

    feat: add product types

--- src/types/type.ts
+++ src/types/type.ts
@@ -14,3 +14,32 @@ export type AddressType = 'billing' | 'shipping';
 export type NavObjType = {
   [key: string]: { text: string; routing: string; obj?: HTMLElement };
 };
+
+export type ProductOptions = {
+  name: string;
+  description?: string;
+  masterVariant: ProductVariant;
+  variants: ProductVariant[];
+};
+
+export type ProductVariant = {
+  id: number;
+  attributes: Attribute[];
+  images: Images[];
+  prices: [];
+};
+
+type Attribute = {
+  name: string;
+  value: number | AttrValue | string;
+};
+
+export type AttrValue = {
+  key: string;
+  label: string;
+};
+type Images = {
+  dimensions: object;
+  label: string;
+  url: string;
+};
