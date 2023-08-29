a2cfd8900fc629cec06d8a3f9fd4b7bd8247063a
Martynjuk Ivan <martiniukgomel@gmail.com>
2023-08-29 16:55:08 +0300

    add adaptive to catalog page 29 авг 2023 г. 16:55:08

--- src/view/pages/catalog/catalog.ts
+++ src/view/pages/catalog/catalog.ts
@@ -1,9 +1,26 @@
 import items from './items';
 import filters from './filters';
+import filterImg from '../../../assets/images/filter-ico.png';
+import activeFilterImg from '../../../assets/images/filter-ico-active.png';
 
 const productsPage = document.createElement('div');
 productsPage.classList.add('products-page');
 
-productsPage.append(filters, items);
+const iconBlock = document.createElement('div');
+iconBlock.classList.add('filters__icon-block');
+
+const icon = document.createElement('img');
+icon.classList.add('icon-block__icon');
+icon.src = filterImg;
+icon.alt = 'filter-icon';
+
+const activeIcon = document.createElement('img');
+activeIcon.classList.add('icon-block__icon');
+activeIcon.src = activeFilterImg;
+activeIcon.alt = 'active-filter-icon';
+
+iconBlock.append(icon, activeIcon);
+
+productsPage.append(filters, items, iconBlock);
 
 export default productsPage;
