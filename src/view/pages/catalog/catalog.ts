e811288fcd49d84f23534bcee207a61cf237c25a
Martynjuk Ivan <martiniukgomel@gmail.com>
2023-08-29 04:00:07 +0300

    add filters html 29 авг 2023 г.  4:00:07

--- src/view/pages/catalog/catalog.ts
+++ src/view/pages/catalog/catalog.ts
@@ -1,29 +1,8 @@
-import categoryLogoObj from '../../../model/data/images-src';
 import items from './items';
-
-const categoryArr = ['Honda', 'Kawasaki', 'Yamaha', 'Suzuki'];
+import filters from './filters';
 
 const productsPage = document.createElement('div');
 productsPage.classList.add('products-page');
-
-const categorySection = document.createElement('div');
-categorySection.classList.add('products-page__category');
-
-categoryArr.forEach((el) => {
-  const logo = document.createElement('img');
-  logo.classList.add('category__logo');
-  logo.src = categoryLogoObj[el];
-  logo.alt = `${el}-logo`;
-
-  categorySection.append(logo);
-});
-
-// products
-
-const filters = document.createElement('div');
-filters.classList.add('catalog__filters');
-
-// Cards
 
 productsPage.append(filters, items);
 
