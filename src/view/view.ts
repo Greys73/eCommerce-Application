b48fa48148c39cabf65374945d25050ca9145af6
Katsiaryna <katsiaryna.n@gmail.com>
2023-09-02 18:45:29 +0300

    fix: delete comments

--- src/view/view.ts
+++ src/view/view.ts
@@ -1,7 +1,5 @@
 import { header } from './header/header';
 import footer from './footer/footer';
-// import productPage from './pages/product/product';
-import { fillProductPage } from '../controller/fillProductPage';
 
 const main = document.createElement('main');
 main.classList.add('main');
@@ -10,9 +8,6 @@ const mainContainer = document.createElement('div');
 mainContainer.classList.add('main__container');
 
 main.append(mainContainer);
-
-// temporary
-fillProductPage('honda-cb400sf-red');
 
 document.body.append(header, main, footer);
 
