ae2be6bd95b7a13576b249c752f970f1fc8524f1
Katsiaryna <katsiaryna.n@gmail.com>
2023-09-04 15:09:57 +0300

    feat: add filters reset on search

--- src/controller/searchHandler.ts
+++ src/controller/searchHandler.ts
@@ -1,9 +1,11 @@
 import { getProductsBySearchField } from '../model/api/apiRoot';
+import filters from '../view/pages/catalog/filters';
 import { searchInput } from '../view/pages/catalog/items';
 import { placeCards } from './filterSubmitHandler';
 
 const searchHandler = async () => {
   const searchValue = searchInput.value;
+  filters.reset();
   try {
     const cardsData = await getProductsBySearchField(searchValue.toLowerCase());
     placeCards(cardsData);
