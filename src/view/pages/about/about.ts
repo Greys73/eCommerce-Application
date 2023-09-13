9b2e500cee8b0c31924a7f3d96ae60edcc4f0aab
Martynjuk Ivan <martiniukgomel@gmail.com>
2023-09-14 00:53:28 +0300

    feat: add rss link 14 сен 2023 г.  0:53:28

--- src/view/pages/about/about.ts
+++ src/view/pages/about/about.ts
@@ -1,3 +1,20 @@
+import '../../../assets/styles/pages/about.scss';
+
+import logoRSSimg from '../../../assets/images/rs_school.png';
+
 const aboutPage = document.createElement('div');
+
+const rssLink = document.createElement('a');
+rssLink.classList.add('about__rss-link');
+rssLink.href = 'https://rs.school/js/';
+
+const rssLogo = document.createElement('img');
+rssLogo.classList.add('about__rss-img');
+rssLogo.alt = 'RSS Logo';
+rssLogo.src = logoRSSimg;
+
+rssLink.append(rssLogo);
+
+aboutPage.append(rssLink);
 
 export default aboutPage;
