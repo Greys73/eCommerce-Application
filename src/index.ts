17f1ca4ab91e5f08f42a96d87c37fc5833f661f4
Katsiaryna <katsiaryna.n@gmail.com>
2023-09-15 14:55:43 +0300

    fix: add temporary view in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,5 @@
 import './view/view';
 import './controller/controllers';
+import { fillBasket } from './controller/fillBasket'; // !!! temporary - remove after adding routing
+
+fillBasket(); // !!! temporary - remove after adding routing
