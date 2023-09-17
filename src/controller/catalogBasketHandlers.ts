1727ff97ba2c7d4eef585fc8d13a3b81cbcaaec3
Greys73 <v.myazin@gmail.com>
2023-09-17 10:08:10 +0400

    feat: popup message after adding to cart Sun, Sep 17, 2023 10:08:10 AM

--- src/controller/catalogBasketHandlers.ts
+++ src/controller/catalogBasketHandlers.ts
@@ -1,6 +1,7 @@
 import { Cart } from '@commercetools/platform-sdk';
 import { addToCart, createCart, getActiveCart } from '../model/api/cartApiRoot';
 import loading from '../assets/images/icons/loading_2.gif';
+import { showResultMessage } from './application';
 
 const loader = new Image();
 loader.src = loading;
@@ -25,7 +26,12 @@ export async function clickBasketBtn(e: Event) {
   }
   const { id, version } = activeCart.body;
   const cart = await addToCart(id, version, btn.name);
-  if (cart.statusCode === 200) disableBasketBtn(btn);
+  if (cart.statusCode === 200) {
+    disableBasketBtn(btn);
+    showResultMessage(cart, 'Product added to cart!');
+  } else {
+    showResultMessage(cart, cart.body.message);
+  }
   loader.remove();
 }
 
